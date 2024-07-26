5a5791e58553c8ab57c94770d949624ed8306288
Fiko Aditama <fiko@simise.id>
2024-07-26 23:07:18 +0700

    Update Task Sesi-10

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 git init .
 git add .
 git commit -m "Update Task Sesi-10"
-git branch -M sesi-10
+git branch -M main
 git remote add origin git@github.com:fico-aditama/mad-flutter.git
-git push -u origin sesi-10 -f
+git push -u origin main -f
